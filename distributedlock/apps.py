a2a1bc0202a1ae36e3d179d44b5a31035a48e266
张云 <easypal@qq.com>
2022-10-24 15:29:52 +0800

    gettext_lazy compatibility

--- distributedlock/apps.py
+++ distributedlock/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
-from django.utils.translation import ugettext_lazy as _
 
+if django.VERSION[0] >= 3:
+    from django.utils.translation import gettext_lazy as _
+else:
+    from django.utils.translation import ugettext_lazy as _
 
 class DistributedLockAppConfig(AppConfig):
     name = 'distributedlock'
